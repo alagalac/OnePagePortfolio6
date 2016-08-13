9864ff8f648238fd157a6ba24d50b8fa3cb90f68
Jason Govenlock <jason@govenlock.com>
2016-08-13 22:56:14 +1200

    basic scrolling

---
+++ app.js
@@ -0,0 +1,47 @@
+
+/*------------------------------------------------------------------
+ Document ready
+ ------------------------------------------------------------------*/
+
+
+
+
+/*------------------------------------------------------------------
+ Scrolling and navigation
+ ------------------------------------------------------------------*/
+
+ $(document).ready(function () {
+    $(document).on("scroll", onScroll);
+
+    $('.header__navigation a').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.header__navigation a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.header__navigation ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+    });
+}
